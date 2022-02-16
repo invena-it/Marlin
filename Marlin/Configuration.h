38199af67ba944013fae74ccc2cf250459cb7970
invena-it <64826396+invena-it@users.noreply.github.com>
2022-02-16 11:00:29 +0100

    UPD Steps adjustments

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -931,7 +931,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.4, 80.4, 403.1, 100.7 }
 
 /**
  * Default Max Feed Rate (mm/s)
